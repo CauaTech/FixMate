1120cbcb361489c2fc96bf7f38d62e0a1d700ea3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-10 20:00:47 -0300

    Vou separar o modal, em um sistema php diferente

---
+++ assents/app/modals/orcamento-modal.php
@@ -0,0 +1,44 @@
+<div class="modal fade fix-modal" id="garar_orcamento">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+
+      <div class="modal-header modal-fheader">
+        <h4 class="modal-title">Gerar Orçamento</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <div class="modal-body">
+    	<div class="row">
+    		<div class="col-md-6">
+    			<div class="modal-inputs">
+    				<label class="title-form">Cliente</label>
+    				<select class="form-select form-modal">
+					  <option>1</option>
+					  <option>2</option>
+					  <option>3</option>
+					  <option>4</option>
+					</select>
+    			</div>
+    		</div>
+    		<div class="col-md-6">
+    			<div class="modal-inputs">
+    				<label class="title-form">Modelo</label>
+    				<input type="text" class="form-control form-modal" name="">
+    			</div>
+    		</div>
+    		<div class="col-md-6">
+    			<div class="modal-inputs">
+    				<label class="title-form">Senha</label>
+    				<input type="text" class="form-control form-modal" name="">
+    			</div>
+    		</div>
+    	</div>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-modal">Cadastrar Celular</button>
+      </div>
+
+    </div>
+  </div>
+</div>
